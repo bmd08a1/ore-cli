024905cca009d21122506e99f34a261b3be12ecc
Dung Bui <bmd08a1@gmail.com>
2024-08-12 00:06:56 +0700

    reduce retries

--- src/send_and_confirm.rs
+++ src/send_and_confirm.rs
@@ -31,8 +31,8 @@ const MIN_SOL_BALANCE: f64 = 0.005;
 
 const RPC_RETRIES: usize = 0;
 const _SIMULATION_RETRIES: usize = 4;
-const GATEWAY_RETRIES: usize = 150;
-const CONFIRM_RETRIES: usize = 8;
+const GATEWAY_RETRIES: usize = 50;
+const CONFIRM_RETRIES: usize = 4;
 
 const CONFIRM_DELAY: u64 = 500;
 const GATEWAY_DELAY: u64 = 0;
